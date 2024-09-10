3d0d9fec0dcdff9fa1988b722314f17a452d0add
faris-backer <faris.backer@lxisoft.com>
2024-09-10 14:22:08 +0530

    first commit

---
+++ src/main/java/com/ecomerce/faris/FbEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.faris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomerce/faris/FbEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.faris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
